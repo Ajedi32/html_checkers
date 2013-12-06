13c3af7617fdc38aa931e87e5d4d6216dfd457d3
Andrew Meyer <andrewm.bpi@gmail.com>
2013-12-05 20:07:15 -0600

    Add logic for computing legal moves
    Add CheckerBoard#getLegalMoves function, which list legal moves
    (up to one jump) for the piece on a given space.

--- assets/javascript/checkers.js
+++ assets/javascript/checkers.js
@@ -55,6 +55,35 @@ CheckerBoard.prototype.getPiece = function(x, y) {
 CheckerBoard.prototype.setPiece = function(x, y, piece) {
 	return this.board[x][y] = piece;
 };
+CheckerBoard.prototype.getLegalMoves = function(x, y) {
+	if (!this.isValidSpace(x, y)) return [];
+
+	var piece = this.getPiece(x, y);
+	if (piece === null) return [];
+
+	var movementVectors = piece.getMovementVectors();
+	var legalTargets = [];
+
+	for (var movementVector in movementVectors) {
+		movementVector = movementVectors[movementVector];
+
+		var x2 = x + movementVector[0];
+		var y2 = y + movementVector[1];
+		var piece2 = this.getPiece(x2, y2);
+
+		if (piece2 === null) {
+			legalTargets.push([x2, y2]);
+		} else if (piece2 !== undefined && piece2 !== null && piece.owner != piece2.owner) {
+			x2 += movementVector[0];
+			y2 += movementVector[1];
+			piece2 = this.getPiece(x2, y2);
+
+			if (piece2 === null) legalTargets.push([x2, y2]);
+		}
+	}
+
+	return legalTargets;
+};
 
 
 function CheckerPiece(owner, rank) {
@@ -63,6 +92,12 @@ function CheckerPiece(owner, rank) {
 	this.owner = owner;
 	this.rank = rank;
 }
+CheckerPiece.prototype.getMovementVectors = function () {
+	var movementVectors = [];
+	if (this.owner == CheckerBoard.prototype.PLAYERS.RED || this.rank == this.RANKS.KING) movementVectors.push([-1, -1], [1, -1]);
+	if (this.owner == CheckerBoard.prototype.PLAYERS.BLACK || this.rank == this.RANKS.KING) movementVectors.push([-1, 1], [1, 1]);
+	return movementVectors;
+};
 CheckerPiece.prototype.RANKS = {
 	MAN: 0,
 	KING: 1

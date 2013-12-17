f5b9f54b9823dfc701e45a297dc9a1de72db25dd
Andrew Meyer <andrewm.bpi@gmail.com>
2013-12-16 22:26:56 -0600

    Add equal function

--- assets/javascript/checkers.js
+++ assets/javascript/checkers.js
@@ -43,6 +43,21 @@ Function.prototype.withArgs = function() {
 };
 
 
+/*
+* Checkes for equality between two objects or primitives
+*/
+function equal(object1, object2) {
+	return (
+		(object1 === object2) || (
+			object1 !== undefined &&
+			object1 !== null &&
+			typeof object1.equals == "function" &&
+			object1.equals(object2)
+		)
+	);
+}
+
+
 function CheckersGame(options) {
 	options = (typeof options == 'undefined') ? {} : options;
 
